194cab81e9c042d6624a6d763944672d94d03002
goran <gorankami@gmail.com>
2017-01-03 12:30:50 +0100

    Added image cache

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 var $     = require("jquery"),
+    icons = require("./icons"),
     Table = require("./table");
 
 var defaults = {
@@ -7,7 +8,21 @@ var defaults = {
   mines  : 10
 };
 
+
 var tableElement = $('#table');
 var table        = new Table();
 table.init(tableElement, defaults.rows, defaults.columns, defaults.mines);
 table.render();
+
+//cache images, prevents white surfaces on clicks
+cache(icons.blank);
+cache(icons.pressed);
+cache(icons.exposedBomb);
+cache(icons.explodedBomb);
+cache(icons.flag);
+icons.bombs.forEach(cache);
+
+function cache(url) {
+  var img = new Image();
+  img.src = url;
+}
